15724691a2b22317c82457ac4fbb45e31149ad6b
Anselmo5 <anselmohenrique191@gmail.com>
2023-07-31 14:27:07 -0300

    alteração

--- etapa5/src/components/Myform2.jsx
+++ etapa5/src/components/Myform2.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react"
 import styles from "./Myform2.module.css"
 
 const Myform2 = () => {
+
+  const [email,setemail] =useState("")
+
+  console.log(email)
   return (
     <div  className={styles.aling}>
       {/* Label envolvendo o input, onde aqui não temos o htmlfor que fazia o nosso link ncomo input*/}
       <label className={styles.form}> 
-        <span>Nome:</span>
-        <input type="Nome"placeholder="digite o seu nome"  className={styles.input}/>
+        <span>Nome:</span> {/*state inline */} {/* Aqui temos um codigo mais simplificado de gerenciamento de dados dentro de um input, onde não foi utilizamos uma função para puchar os dados, mas sim incerindo direto na tag */}
+        {/* Que se torna mais simples, para ocasioes onde você precisa pegar os dados */}
+        <input type="Nome"placeholder="digite o seu nome"  className={styles.input} onChange={(e) => setemail(e.target.value)}/>
         <input type="email"placeholder="digite o email"  className={styles.input}/>
         <input type="submit" value="enviar"  className={styles.input}/>
       </label>
