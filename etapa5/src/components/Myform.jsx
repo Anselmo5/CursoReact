acd083434260962c572f598a14a2f0f102816d3a
Anselmo5 <anselmohenrique191@gmail.com>
2023-07-31 14:15:37 -0300

    alteração

--- etapa5/src/components/Myform.jsx
+++ etapa5/src/components/Myform.jsx
@@ -3,13 +3,20 @@ import styles from "./Myform.module.css"
 
 const Myform = () => {
 
- const [nome,setnome] = useState("Anselmo")
+ const [nome,setnome] = useState("Anselmo") //Gerenciamento de dados de um input
+ const [ganch, setganch] = useState("")
+
+
+ const handelername = (e) => {
+  setganch(e.target.value) //definimos o elemento dentro do input que queremos pegar
+ }
+console.log(ganch); // podemos ver o elemento dentro do input
 
   return (
     <div>
       <form className={styles.form}>
         <label htmlFor="name">Nome:{nome}</label>
-        <input type="text"  name="name" placeholder="digiter um nome"/>
+        <input type="text"  name="name" placeholder="digiter um nome" onChange={handelername}/> {/*onChange e um evento que acontece quando uma tecla e precionada dentro do input */}
         <input type="submit" value="Enviar" className={styles.input} onClick={() => setnome("Gabriel")}/> 
       </form>
     </div>
